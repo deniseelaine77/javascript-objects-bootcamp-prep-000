f9908609b48a07ecb168ef304af580e5c8ff95b8
dehast59@gmail.com <dehast59@gmail.com>
2018-09-06 15:32:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(object, artistName, songTitle) {
   object[artistName] = songTitle
   return object
 }
+
+function removeFromPlaylist(object, artistName) {
+  delete object.artistName
+  return object
+}
